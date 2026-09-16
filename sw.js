22b659863ca80e70178fd5793a14a89df45803e0
david-mullins-tfs <101726782+david-mullins-tfs@users.noreply.github.com>
2026-09-16 13:44:55 -0400

    v16
    Restored Macro history and set weight issure

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='barbarian-bulk-v15';
+const CACHE='barbarian-bulk-v16';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./nutrition.js','./cardio.js','./volume.js','./rest-timer.js','./warmup.js','./weight-sync.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
